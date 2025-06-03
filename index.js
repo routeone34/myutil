b1e3ab31cf56fc64a535c974090aaccee276a4f0
ykuwagaki <ykuwagaki@gmail.com>
2025-06-03 12:08:47 +0900

    a

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { dateUtil } from "./sub/dateUtil/lib/dateUtil.js";
 import { sqlcl } from "./sub/sqlcl/lib/sqlcl.js";
+import { s3wrap } from "./sub/s3wrap/lib/s3wrap.js";
+import { uzwrap } from "./sub/uzwrap/lib/uzwrap.js"; 
 
-export { dateUtil, sqlcl }
+export { dateUtil, sqlcl, s3wrap, uzwrap }
